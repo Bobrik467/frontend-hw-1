59df3e279564fa8ffab439d5a12ec8f3ac5f3dfb
Bobrik467 <petyatyiu@yandex.ru>
2026-02-06 23:39:02 +0300

    Update pow.ts

--- src/pow.ts
+++ src/pow.ts
@@ -1,3 +1,21 @@
-const pow = () => {};
+function checkNumber(value) {
+  if (typeof value !== 'number') {
+    throw new Error('INVALID_ARGUMENT');
+  }
+}
+
+const pow = (base, exponent) => {
+  checkNumber(base);
+  
+  if (exponent !== undefined) {
+    checkNumber(exponent);
+    return base ** exponent
+  }
+  
+  return function(exponent) {
+    checkNumber(exponent);
+    return base ** exponent
+  }
+};
 
 export default pow;
