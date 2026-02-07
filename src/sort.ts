f2e408b799cd561fd7c3c1e10afa018a4e05d761
Bobrik467 <petyatyiu@yandex.ru>
2026-02-07 21:57:10 +0300

    Update sort.ts

--- src/sort.ts
+++ src/sort.ts
@@ -1,3 +1,19 @@
-const sort = () => {};
-
+const sort = (str) => {
+  if (typeof(str) !== 'string') {
+    throw new Error('INVALID_ARGUMENT')
+  }
+  
+  return str
+          .toLowerCase()
+          .split(' ')
+          .map(word => 
+              word
+               .split('')
+               .sort()
+               .join('')
+              )
+              .sort((a, b) => a.length - b.length)
+              .join(' ');
+  
+};
 export default sort;
